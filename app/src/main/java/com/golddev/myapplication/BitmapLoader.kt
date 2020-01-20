4c84ac78212fa7b87f161e4370e4442967c10149
dominikgold <goldberg.dominik@gmail.com>
2020-01-20 10:51:26 +0100

    adds BitmapLoader - custom implementation for loading local images

---
+++ app/src/main/java/com/golddev/myapplication/BitmapLoader.kt
@@ -0,0 +1,142 @@
+package com.golddev.myapplication
+
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import androidx.core.graphics.rotationMatrix
+import androidx.exifinterface.media.ExifInterface
+import kotlin.math.roundToInt
+
+class BitmapLoader(private val absoluteFilePath: String) {
+
+    // TODO we should do the loading of bitmaps asynchronously
+
+    private val rotation: Int by lazy {
+        val orientation = ExifInterface(absoluteFilePath).getAttributeInt(
+            ExifInterface.TAG_ORIENTATION,
+            ExifInterface.ORIENTATION_NORMAL
+        )
+        return@lazy when (orientation) {
+            ExifInterface.ORIENTATION_ROTATE_270 -> 270
+            ExifInterface.ORIENTATION_ROTATE_180 -> 180
+            ExifInterface.ORIENTATION_ROTATE_90 -> 90
+            else -> 0
+        }
+    }
+
+    fun loadForMaximalSize(maximalWidth: Int, maximalHeight: Int): Bitmap {
+        if (maximalWidth <= 0 || maximalHeight <= 0) {
+            throw IllegalArgumentException("Width and height must be > 0 when loading a scaled bitmap")
+        }
+
+        val initialBitmap = loadScaledBitmap(maximalWidth, maximalHeight)
+        val aspectRatio = initialBitmap.aspectRatio
+
+        // Here we calculate values for the resulting width and height, so we conform to both the given minimalWidth and minimalHeight
+        // while staying true to the initial aspect ratio of the image
+        val (resultWidth, resultHeight) = fitMaximalDimensionsForAspectRatio(
+            maximalWidth,
+            maximalHeight,
+            aspectRatio
+        )
+        val resultBitmap = Bitmap.createScaledBitmap(initialBitmap, resultWidth, resultHeight, true)
+        if (resultBitmap !== initialBitmap) {
+            initialBitmap.recycle()
+        }
+        return resultBitmap.applyExifRotation()
+    }
+
+    private fun decodeScaledBitmapOptions(
+        minimalWidth: Int,
+        minimalHeight: Int,
+        rotation: Int
+    ): BitmapFactory.Options =
+        BitmapFactory.Options().apply {
+            inJustDecodeBounds = true
+            BitmapFactory.decodeFile(absoluteFilePath, this)
+
+            val isRotated = rotation % 180 != 0
+            val imageWidth = if (isRotated) this.outHeight else this.outWidth
+            val imageHeight = if (isRotated) this.outWidth else this.outHeight
+
+            val scaleFactor =
+                calculateInSampleSize(minimalWidth, minimalHeight, imageWidth, imageHeight)
+            inSampleSize = scaleFactor
+            inJustDecodeBounds = false
+        }
+
+    private fun loadScaledBitmap(minimalWidth: Int, minimalHeight: Int): Bitmap {
+        val bitmapOptions = decodeScaledBitmapOptions(minimalWidth, minimalHeight, rotation)
+
+        return BitmapFactory.decodeFile(absoluteFilePath, bitmapOptions)
+    }
+
+    /**
+     * Calculates the scale factor to use when setting inSampleSize on [BitmapFactory.Options] for loading scaled Bitmaps into memory. The
+     * result will always be a power of 2, as per the documentation.
+     * @param targetWidth The minimal width of the resulting bitmap.
+     * @param targetHeight The minimal height of the resulting bitmap.
+     * @param imageWidth The original width of the bitmap.
+     * @param imageHeight The original height of the bitmap.
+     */
+    private fun calculateInSampleSize(
+        targetWidth: Int,
+        targetHeight: Int,
+        imageWidth: Int,
+        imageHeight: Int
+    ): Int {
+        var result = 1
+        while (imageWidth / (result * 2) >= targetWidth && imageHeight / (result * 2) >= targetHeight) {
+            result *= 2
+        }
+        return result
+    }
+
+    /**
+     * Creates a new Bitmap that is equal to the source bitmap rotated by the given amount of rotation.
+     */
+    private fun Bitmap.applyExifRotation(): Bitmap {
+        var resultBitmap = this
+        if (rotation > 0) {
+            resultBitmap = Bitmap.createBitmap(
+                this,
+                0,
+                0,
+                this.width,
+                this.height,
+                rotationMatrix(rotation.toFloat()),
+                true
+            )
+            this.recycle()
+        }
+        return resultBitmap
+    }
+
+    private val Bitmap.aspectRatio: Float
+        get() = width.toFloat() / height.toFloat()
+
+    fun fitMaximalDimensionsForAspectRatio(
+        maximalWidth: Int,
+        maximalHeight: Int,
+        aspectRatio: Float
+    ): IntArray =
+        if (aspectRatio > 1f) {
+            if (maximalHeight * aspectRatio < maximalWidth) {
+                aspectRatio.fitDimensionsByHeight(maximalHeight)
+            } else {
+                aspectRatio.fitDimensionsByWidth(maximalWidth)
+            }
+        } else {
+            if (maximalWidth / aspectRatio < maximalHeight) {
+                aspectRatio.fitDimensionsByWidth(maximalWidth)
+            } else {
+                aspectRatio.fitDimensionsByHeight(maximalHeight)
+            }
+        }
+
+    private fun Float.fitDimensionsByHeight(height: Int) =
+        intArrayOf((height * this).roundToInt(), height)
+
+    private fun Float.fitDimensionsByWidth(width: Int) =
+        intArrayOf(width, (width / this).roundToInt())
+
+}
--- app/src/main/java/com/golddev/myapplication/MainActivity.kt
+++ app/src/main/java/com/golddev/myapplication/MainActivity.kt
@@ -1,17 +1,22 @@
 package com.golddev.myapplication
 
 import android.app.Activity
+import android.content.ContentValues
 import android.content.Intent
 import android.net.Uri
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
 import android.provider.MediaStore
 import androidx.core.content.FileProvider
 import coil.api.load
 import coil.size.Scale
 import kotlinx.android.synthetic.main.activity_main.*
 import java.io.File
+import java.io.FileInputStream
+import java.io.FileOutputStream
+import java.text.SimpleDateFormat
 import java.util.*
 
 private const val FILE_PROVIDER_AUTHORITY = "com.golddev.myapplication.fileprovider"
@@ -20,6 +25,7 @@ private const val CACHE_FOLDER = ".CoilExifRotationTest"
 class MainActivity : AppCompatActivity() {
 
     private var imageUri: Uri? = null
+    private var imageFilePath: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,7 +34,9 @@ class MainActivity : AppCompatActivity() {
         start_camera_button.setOnClickListener {
             val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE).apply {
                 val file = createCacheImageFile()
-                val fileUri = FileProvider.getUriForFile(applicationContext, FILE_PROVIDER_AUTHORITY, file)
+                val fileUri =
+                    FileProvider.getUriForFile(applicationContext, FILE_PROVIDER_AUTHORITY, file)
+                imageFilePath = file.absolutePath
                 imageUri = fileUri
                 putExtra(MediaStore.EXTRA_OUTPUT, fileUri)
                 if (Build.VERSION.SDK_INT >= 23) {
@@ -44,8 +52,17 @@ class MainActivity : AppCompatActivity() {
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         if (requestCode == 10 && resultCode == Activity.RESULT_OK) {
-            image_view.load(imageUri) {
+            // load the taken image with coil
+            image_view_coil.load(imageUri) {
                 scale(Scale.FIT)
+            }
+            // load the taken image with custom implementation
+            imageFilePath?.let { imageFilePath ->
+                val bitmap = BitmapLoader(imageFilePath).loadForMaximalSize(
+                    maximalWidth = image_view_bitmap_loader.width,
+                    maximalHeight = image_view_bitmap_loader.height
+                )
+                image_view_bitmap_loader.setImageBitmap(bitmap)
             }
         }
         super.onActivityResult(requestCode, resultCode, data)
@@ -58,4 +75,5 @@ class MainActivity : AppCompatActivity() {
         }
         return File.createTempFile(UUID.randomUUID().toString(), ".jpg", directory)
     }
+
 }
